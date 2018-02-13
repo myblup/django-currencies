116813158bd9f94b5eb4fea91b99b5b6670590f0
Emile <emile@blookup.com>
2018-02-13 09:51:18 +0100

    add get_currency_symbol and externalize calculation of price

--- currencies/utils.py
+++ currencies/utils.py
@@ -5,17 +5,17 @@ from decimal import Decimal as D #, ROUND_UP
 from .models import Currency as C
 from .conf import SESSION_KEY
 
-
-def calculate(price, code):
-    to, default = C.active.get(code=code), C.active.default()
-
-    # First, convert from the default currency to the base currency,
-    # then convert from the base to the given currency
-    price = (D(price) / default.factor) * to.factor
-
+def do_calculate(price, currency):
+    default = C.active.default()
+    price = (D(price) / default.factor) * currency.factor
     # rounding turns 6.90 into 6.91 :duh:
     # the default HALF_EVEN rounding works fine so...
     return price.quantize(D("0.01")) #, rounding=ROUND_UP)
+
+
+def calculate(price, code):
+    to = C.active.get(code=code)
+    return do_calculate(price, to)
 
 
 def convert(amount, from_code, to_code):
@@ -30,16 +30,26 @@ def convert(amount, from_code, to_code):
     return amount.quantize(D("0.01")) #, rounding=ROUND_UP)
 
 
-def get_currency_code(request):
+def get_currency(request):
     for attr in ('session', 'COOKIES'):
         if hasattr(request, attr):
             try:
-                return getattr(request, attr)[SESSION_KEY]
+                return C.active.get(code=getattr(request, attr)[SESSION_KEY])
             except KeyError:
                 continue
 
     # fallback to default...
     try:
-        return C.active.default().code
+        return C.active.default()
     except C.DoesNotExist:
         return None  # shit happens...
+
+def get_currency_code(request):
+    currency = get_currency(request)
+    return currency.code if currency else None
+
+
+def get_currency_symbol(request):
+    """ return the currency symbol """
+    currency = get_currency(request)
+    return currency.symbol if currency else None
