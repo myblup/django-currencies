ca6ae9e048dec580a2a157d87c13d635aaf96244
Bigb <bruno@blookup.com>
2018-07-02 10:06:50 +0200

    reordered the fields in the list_display + added 'activate' and 'deactivate' admin actions

--- currencies/admin.py
+++ currencies/admin.py
@@ -3,10 +3,29 @@ from django.contrib import admin
 
 from .models import Currency
 
+def annotate(description):
+    def wrapper(func):
+        func.short_description = description
+        return func
+    return wrapper
+
+@annotate("activate selected currencies")
+def activate(modeladmin, request, queryset):
+    queryset.update(is_active=True)
+
+@annotate("deactivate selected currencies")
+def deactivate(modeladmin, request, queryset):
+    queryset.update(is_active=False)
+
+
+
 
 class CurrencyAdmin(admin.ModelAdmin):
-    list_display = ("name", "is_active", "is_base", "is_default", "code", "symbol", "factor")
+    list_display = ("name", "code", "symbol", "is_active", "is_base", "is_default",  "factor")
     list_filter = ("is_active", )
     search_fields = ("name", "code")
+
+    actions = [activate, deactivate]
+
 
 admin.site.register(Currency, CurrencyAdmin)
